d244dc97fdeb05e091e975443b264ccf90137397
ANGEL_TADEO_DE_LEON_CENICEROS <a.22308051281165@cbtis128.edu.mx>
2025-03-06 19:44:31 +0000

    DeLeon_ElevatedButtons

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,117 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MisBotones());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class MisBotones extends StatelessWidget {
+  const MisBotones({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("De Leon Mat: 22308051281165"),
+          titleTextStyle: const TextStyle(color: Colors.white, fontSize: 20),
+          centerTitle: true,
+          backgroundColor: Color(0xff52a67b),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton.icon(
+                onPressed: () {},
+                icon: const Icon(Icons.add, size: 30, color: Color(0xff429a45)),
+                label: const Text("Boton elevado",
+                    style: TextStyle(fontSize: 20, color: Color(0xff429a45))),
+                style: ElevatedButton.styleFrom(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 15, horizontal: 30),
+                  backgroundColor: Colors.lightGreen[100],
+                ),
+              ),
+              const SizedBox(height: 15),
+              FilledButton.icon(
+                onPressed: () {},
+                icon: const Icon(Icons.check, size: 30),
+                label:
+                    const Text("Boton relleno", style: TextStyle(fontSize: 20)),
+                style: FilledButton.styleFrom(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 15, horizontal: 30),
+                  backgroundColor: Colors.green,
+                ),
+              ),
+              const SizedBox(height: 15),
+              FilledButton.icon(
+                onPressed: () {},
+                icon: const Icon(Icons.star, size: 30),
+                label: const Text("Boton relleno con tono",
+                    style: TextStyle(fontSize: 20)),
+                style: FilledButton.styleFrom(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 15, horizontal: 30),
+                  backgroundColor: Colors.lightGreen,
+                ),
+              ),
+              const SizedBox(height: 15),
+              OutlinedButton.icon(
+                onPressed: () {},
+                icon:
+                    const Icon(Icons.info, size: 30, color: Color(0xff429a45)),
+                label: Text("Boton Delineado",
+                    style: TextStyle(fontSize: 20, color: Color(0xff429a45))),
+                style: OutlinedButton.styleFrom(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 15, horizontal: 30),
+                  side: const BorderSide(color: Colors.green),
+                ),
+              ),
+              const SizedBox(height: 15),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text("Boton 5",
+                    style: TextStyle(fontSize: 20, color: Color(0xff429a45))),
+                style: ElevatedButton.styleFrom(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 15, horizontal: 30),
+                  backgroundColor: Colors.lightGreen[100],
+                ),
+              ),
+              SizedBox(height: 15),
+              FilledButton(
+                onPressed: () {},
+                child: Text("Boton 6", style: TextStyle(fontSize: 20)),
+                style: FilledButton.styleFrom(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 15, horizontal: 30),
+                  backgroundColor: Colors.green,
+                ),
+              ),
+              const SizedBox(height: 15),
+              FilledButton(
+                onPressed: () {},
+                child: Text("Boton 7", style: TextStyle(fontSize: 20)),
+                style: FilledButton.styleFrom(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 15, horizontal: 30),
+                  backgroundColor: Colors.lightGreen,
+                ),
+              ),
+              const SizedBox(height: 15),
+              OutlinedButton(
+                onPressed: () {},
+                child: Text("Boton 8",
+                    style: TextStyle(fontSize: 20, color: Color(0xff429a45))),
+                style: OutlinedButton.styleFrom(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 15, horizontal: 30),
+                  side: const BorderSide(color: Colors.green),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
